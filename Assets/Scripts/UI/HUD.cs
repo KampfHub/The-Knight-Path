468aced91b1845f51cb95d9a24b745f79dbaa17e
KampfHub <108213505+KampfHub@users.noreply.github.com>
2023-03-23 21:09:31 +0300

    HUD: change border on timer

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -1,6 +1,6 @@
 using UnityEngine.UI;
 using UnityEngine;
-
+using System.Collections;
 
 public class HUD : MonoBehaviour
 {
@@ -8,9 +8,7 @@ public class HUD : MonoBehaviour
     [SerializeField] private Image[] _iconBorders;
     private Color[] setAlfa = new Color[6];
     private GameObject playerRef, hpBar, defenseBar;
-    //private float ActionTime = 0.0f;
-       
-        private void Awake()
+    private void Awake()
     {
         playerRef = GameObject.FindWithTag("Player");
         hpBar = GameObject.Find("HPBar");
@@ -21,15 +19,22 @@ public class HUD : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (Time.time > nextActionTime)
-        //{
-        //    nextActionTime += period;
-        //    Debug.Log("Secces");
-        //}
-        //Debug.Log(Time.time);
+
     }
+
     private void Start()
     {
+        
+    }
+    private IEnumerator ChangeBorderOnTimer(int slot, float duration)
+    {
+        float fillValue = duration;
+        while (duration > 0)
+        {
+            duration -= Time.deltaTime;
+            _iconBorders[slot].fillAmount = duration/fillValue;
+            yield return null;
+        }
     }
     public void ÑhangeWidgetValue()
     {
@@ -46,12 +51,12 @@ public class HUD : MonoBehaviour
     {
         switch (impactName)
         {
-            case "JumpForce": ShowIcon(0, 1); Invoke("HideIconJumpForce", duration); break;
-            case "Speed": ShowIcon(1, 1); Invoke("HideIconSpeed", duration); break;
-            case "Poison": ShowIcon(2, 1); Invoke("HideIconBrokenSword", duration); break;
-            case "Power": ShowIcon(3, 1); Invoke("HideIconPower", duration); break;
-            case "Trap": ShowIcon(4, 1); Invoke("HideIconLowSpeed", duration); break;
-            case "Immortal": ShowIcon(5, 1); Invoke("HideIconImmortal", duration); break;
+            case "JumpForce": ShowIcon(0, 1); Invoke("HideIconJumpForce", duration); StartCoroutine(ChangeBorderOnTimer(0, duration)); break;
+            case "Speed": ShowIcon(1, 1); Invoke("HideIconSpeed", duration); StartCoroutine(ChangeBorderOnTimer(1, duration)); break;
+            case "Poison": ShowIcon(2, 1); Invoke("HideIconBrokenSword", duration); StartCoroutine(ChangeBorderOnTimer(2, duration)); break;
+            case "Power": ShowIcon(3, 1); Invoke("HideIconPower", duration); StartCoroutine(ChangeBorderOnTimer(3, duration)); break;
+            case "Trap": ShowIcon(4, 1); Invoke("HideIconLowSpeed", duration); StartCoroutine(ChangeBorderOnTimer(4, duration)); break;
+            case "Immortal": ShowIcon(5, 1); Invoke("HideIconImmortal", duration); StartCoroutine(ChangeBorderOnTimer(5, duration)); break;
         }
     }
     public void ShowIcon(int slot, int transparent)
@@ -62,10 +67,8 @@ public class HUD : MonoBehaviour
         _iconSlots[slot].color = setAlfa[slot];
         _iconBorders[slot].color = setAlfa[slot];
     }
-    private void ShowBorderInTick(int slot, float duration)
-    {
-        //_iconBorders[slot].fillAmount
-    }
+
+
     private void HideAllIcons()
     {
         for(int i = 0; i< _iconSlots.Length; i++)
