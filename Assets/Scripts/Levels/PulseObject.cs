6826a7f7f9a1df4901e7a7fdbcd157063cefe956
KampfHub <108213505+KampfHub@users.noreply.github.com>
2023-04-11 23:03:19 +0300

    level 8 complite

---
+++ Assets/Scripts/Levels/PulseObject.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PulseObject : MonoBehaviour
+{
+    [SerializeField] private float _upperBound;
+    [SerializeField] private float _lowerBound;
+    bool upscale;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.localScale.x >= _upperBound) upscale = false;
+        if (gameObject.transform.localScale.x <= _lowerBound) upscale = true;
+        SetScale();
+        Debug.Log(transform.localScale);
+    }
+    private void SetScale()
+    {
+        transform.localScale = new Vector3(GetAxisValue(transform.localScale.x),
+           GetAxisValue(transform.localScale.y), GetAxisValue(transform.localScale.z));
+    }
+    private float GetAxisValue(float baseScale)
+    {
+        if (upscale) return baseScale + Time.deltaTime;
+        if (upscale == false) return baseScale - Time.deltaTime;
+        return 0;
+    }
+}
